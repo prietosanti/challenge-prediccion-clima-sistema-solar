81548b59dd77e87d1e446f0014bb27ce762385c0
prietosanti <prietosanti@gmail.com>
2018-10-15 18:03:05 -0300

    Add Test

---
+++ src/test/java/com/prietosanti/model/SistemaSolarTest.java
@@ -0,0 +1,33 @@
+package com.prietosanti.model;
+
+import com.prietosanti.configuration.PredictorConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { PredictorConfiguration.class })
+public class SistemaSolarTest {
+
+    @Autowired
+    private SistemaSolar sistemaSolar;
+
+    @Test
+    public void planetasAlineadosConElCentro() {
+        sistemaSolar.desplazarPlanetas(0);
+        assertTrue(sistemaSolar.planetasAlineados(10));
+        assertTrue(sistemaSolar.planetasAlineadosConElCentro(10));
+    }
+
+    @Test
+    public void planetasNoAlineadosConElCentro() {
+        sistemaSolar.desplazarPlanetas(30);
+        assertFalse(sistemaSolar.planetasAlineados(10));
+        assertFalse(sistemaSolar.planetasAlineadosConElCentro(10));
+    }
+}
